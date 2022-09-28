55f34e462b22e5238c23ca27554d7d72c2c27371
rafaelbba <>
2022-09-28 14:09:09 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h> //testar debug com esse código
+#include <stdlib.h>
+#include <time.h>
+int main() {
+    int i, aleatorio;
+    srand( time(NULL) ); //semente do numero aleatório
+    for(i=1 ; i <= 10 ; i++){
+	    aleatorio = rand()%10;
+	    printf("Numero %d: %d\n",i, aleatorio); //gera números de 0 a 9
+    }
+    return 0;
+}
